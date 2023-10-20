d95ac5a1d2b4d0071dfbe4fe3c77da1441b5fda3
Yuhui Yao <bolus123@users.noreply.github.com>
2023-10-20 00:02:24 -0500

    update

--- R/RMSE.R
+++ R/RMSE.R
@@ -48,6 +48,74 @@ RMSE <- function(Y, Phi, Mu) {
 #' result <- GibbsRFLSM(Y, q, diag(nrow = q), 0.1, 0.1, 0.1, 0.1, 
 #' 1, 1, 0.1, "MonoALASSO", Inf, 0, 1000, 1, 100, 1e-10, H)
 #'
+#' Residual(Y, result$Phi, result$Mu)
+Residual <- function(Y, Phi, Mu) {
+  
+  T <- length(Y)
+  q <- dim(Phi)[1]
+  nsim <- dim(Phi)[2]
+  ee <- matrix(NA, nrow = T - q, ncol = nsim)
+  rss <- rep(0, nsim)
+  
+  for (ii in seq(nsim)) {
+    V <- matrix(Y, ncol = 1) - Mu[, ii]
+    Vas <- getV(V, q)
+    V <- V[-c(1:q)]
+    Vas <- Vas[-c(1:q), ]
+    ee[, ii] <- V - Vas %*% Phi[, ii]
+  }
+  ee
+}
+
+#' obtain the root squared error
+#' 
+#' @param Y is a vector
+#' @param Phi is the coefficient
+#' @param Mu is the mean
+#' @export
+#' @examples
+#' T <- 100
+#' q <- 5
+#' H <- getHMatMT(T, q)
+#' Y <- arima.sim(list(ar = 0.5), n = T)
+#' 
+#' result <- GibbsRFLSM(Y, q, diag(nrow = q), 0.1, 0.1, 0.1, 0.1, 
+#' 1, 1, 0.1, "MonoALASSO", Inf, 0, 1000, 1, 100, 1e-10, H)
+#'
+#' Fit(Y, result$Phi, result$Mu)
+Fit <- function(Y, Phi, Mu) {
+  
+  T <- length(Y)
+  q <- dim(Phi)[1]
+  nsim <- dim(Phi)[2]
+  ff <- matrix(NA, nrow = T - q, ncol = nsim)
+  rss <- rep(0, nsim)
+  
+  for (ii in seq(nsim)) {
+    V <- matrix(Y, ncol = 1) - Mu[, ii]
+    Vas <- getV(V, q)
+    V <- V[-c(1:q)]
+    Vas <- Vas[-c(1:q), ]
+    ff[, ii] <- Vas %*% Phi[, ii] + Mu[-c(1:q), ii]
+  }
+  rbind(Mu[1:q, ], ff)
+}
+
+#' obtain the root squared error
+#' 
+#' @param Y is a vector
+#' @param Phi is the coefficient
+#' @param Mu is the mean
+#' @export
+#' @examples
+#' T <- 100
+#' q <- 5
+#' H <- getHMatMT(T, q)
+#' Y <- arima.sim(list(ar = 0.5), n = T)
+#' 
+#' result <- GibbsRFLSM(Y, q, diag(nrow = q), 0.1, 0.1, 0.1, 0.1, 
+#' 1, 1, 0.1, "MonoALASSO", Inf, 0, 1000, 1, 100, 1e-10, H)
+#'
 #' RSS(Y, result$Phi, result$Mu)
 RSS <- function(Y, Phi, Mu) {
   
--- R/getPvalue.R
+++ R/getPvalue.R
@@ -1,62 +1,7 @@
-#' get the P value for RFLSM with kernel smoothing
-#' 
-#' @param TauGamma is the distributions of shifts.
-#' @param tail is type of the test.
-getPvalueKSRFLSM <- function(TauGamma, tail = "2-sided") {
-  tmp <- TauGamma
-  
-  nn <- dim(tmp)[1]
-  pvalue <- rep(0, nn)
-  
-  for (i in 1:nn) {
-    
-    tmpkde <- density(tmp[i, ])
-    rtmpdens <- spatstat.explore::CDF(tmpkde)
-    tmpp <- rtmpdens(0)
-    
-    if (tail == "2-sided") {
-      pvalue[i] <- 2 * min(1 - tmpp, tmpp)
-    } else if (tail == "left-sided") {
-      pvalue[i] <- tmpp
-    } else if (tail == "right-sided") {
-      pvalue[i] <- 1 - tmpp
-    }
-    
-  }
-  return(pvalue)
-}
-
-
-#' get the P value for RFLSM
-#' 
-#' @param TauGamma is the distributions of shifts.
-#' @param tail is type of the test.
-getPvalueRFLSM <- function(TauGamma, tail = "2-sided") {
-  tmp <- TauGamma
-  
-  nn <- dim(tmp)[1]
-  pvalue <- rep(0, nn)
-  
-  for (i in 1:nn) {
-    
-    if (tail == "2-sided") {
-      pvalue[i] <- mean(tmp[i, ] != 0)
-    } else if (tail == "left-sided") {
-      pvalue[i] <- mean(tmp[i, ] < 0)
-    } else if (tail == "right-sided") {
-      pvalue[i] <- mean(tmp[i, ] > 0)
-    }
-    
-  }
-  return(pvalue)
-}
-
-
 #' get a vector of p values
 #' 
 #' @param TauGamma is the distributions of shifts.
 #' @param tail is type of the test.
-#' @param method get p values with or without kernel smoothing.
 #' @export
 #' @examples
 #' T <- 100
@@ -68,14 +13,24 @@ getPvalueRFLSM <- function(TauGamma, tail = "2-sided") {
 #' 1, 1, 0.1, "MonoALASSO", Inf, 0, 1000, 1, 100, 1e-10, H)
 #'
 #' getPvalue(result$Tau * result$Gamma)
-getPvalue <- function(TauGamma, tail = "2-sided", method = "raw") {
+getPvalue <- function(TauGamma, tail = "2-sided") {
   
-  if (method == "raw") {
-    pvalue <- getPvalueRFLSM(TauGamma, tail)
-  } else if (method == "ks") {
-    pvalue <- getPvalueKSRFLSM(TauGamma, tail) 
+  tmp <- TauGamma
+  
+  nn <- dim(tmp)[1]
+  pvalue <- rep(0, nn)
+  
+  for (i in 1:nn) {
+    
+    if (tail == "2-sided") {
+      pvalue[i] <- 2 * min(mean(tmp[i, ] < 0), mean(tmp[i, ] > 0))
+    } else if (tail == "left-sided") {
+      pvalue[i] <- mean(tmp[i, ] < 0)
+    } else if (tail == "right-sided") {
+      pvalue[i] <- mean(tmp[i, ] > 0)
+    }
+    
   }
-  
-  pvalue
+  return(pvalue)
   
 }
