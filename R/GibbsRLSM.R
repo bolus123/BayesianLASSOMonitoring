5b49456d41043b41c12fd4819af37f8f2ed04d02
Yuhui Yao <bolus123@users.noreply.github.com>
2023-11-13 15:03:00 -0600

    update

--- R/GibbsRLSM.R
+++ R/GibbsRLSM.R
@@ -5,21 +5,21 @@
 #' @param H is the design matrix for shifts.
 #' @param X is the input matrix
 #' @param q is the number of lags.
-#' @param A is a given variance-covariance matrix in MT and regression for the coefficients for autoregressors.
+#' @param A is a given variance-covariance matrix in MT and regression for the slab-and-spike coefficients.
 #' @param a is a given shape of the prior gamma distribution for sigma2.
 #' @param b is a given scale of the prior gamma distribution for sigma2.
 #' @param alpha is a given shape of the prior gamma distribution for lambda2.
-#' @param beta is a given scale of the prior gamma distribution for lambda22.
-#' @param theta1 is a given shape1 of the prior beta distribution for the probability of Tau.
-#' @param theta2 is a given shape2 of the prior beta distribution for the probability of Tau.
+#' @param beta is a given scale of the prior gamma distribution for lambda2.
+#' @param theta1 is a given shape1 of the prior beta distribution for the probability of Tau and Kappa.
+#' @param theta2 is a given shape2 of the prior beta distribution for the probability of Tau and Kappa.
 #' @param xi2 is a given variance of the prior normal distribution for shifts.
 #' @param method is a choice of methods including MT(McCulloch-Tsay), regression, LASSO, ALASSO(Adaptive LASSO), MonoLASSO(LASSO with Monotonicity constrains), MonoALASSO(Adaptive LASSO with Monotonicity constrains).
-#' @param bound0 is a lower bound of the methods with Monotonicity constrains.
-#' @param boundqplus1 is  a upper bound of the methods with Monotonicity constrains.
-#' @param nsim is the number of samples draw from MCMC.
+#' @param bound0 is an upper bound of the methods with Monotonicity constrains.
+#' @param boundqplus1 is  a lower bound of the methods with Monotonicity constrains.
+#' @param nsim is the number of draws from MCMC.
 #' @param by is the interval of systematic sampling for the draws from MCMC.
 #' @param burnin is the length of burn-in period.
-#' @param tol is the tolerance.
+#' @param tol is the tolerance level.
 #' @references McCulloch, R. E., & Tsay, R. S. (1993). Bayesian inference and prediction for mean and variance shifts in autoregressive time series. Journal of the american Statistical association, 88(423), 968-978.
 #' 
 #' 
