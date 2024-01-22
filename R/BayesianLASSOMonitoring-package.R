654b4f9fa3ad7d5c6f8ed15ec4dcf4d46f166a53
Yuhui Yao <bolus123@users.noreply.github.com>
2024-01-21 19:23:07 -0600

    update

--- R/BayesianLASSOMonitoring-package.R
+++ R/BayesianLASSOMonitoring-package.R
@@ -8,7 +8,7 @@
 #'
 #' @name BayesianLASSOMonitoring-package
 #' @references McCulloch, R. E., & Tsay, R. S. (1993). Bayesian inference and prediction for mean and variance shifts in autoregressive time series. Journal of the american Statistical association, 88(423), 968-978.
-#' @import Rcpp VGAM ZINARp pracma
+#' @import Rcpp VGAM ZINARp 
 #' @importFrom stats dnorm
 #' @importFrom stats rnorm
 #' @importFrom stats arima
@@ -16,6 +16,7 @@
 #' @importFrom stats quantile
 #' @importFrom stats sd
 #' @importFrom Rcpp evalCpp
+#' @importFrom pracma Diag
 #' @useDynLib BayesianLASSOMonitoring, .registration = TRUE
 NULL
 
